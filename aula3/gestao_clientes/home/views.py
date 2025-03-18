f38b573ae874da692b0730d8e81153e2aa693c2d
kaique <moraiskaique06@gmail.com>
2025-03-18 15:46:18 -0300

    Aula 8

--- aula3/gestao_clientes/home/views.py
+++ aula3/gestao_clientes/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.generic.base import TemplateView
 
 
 def home(request):
@@ -9,3 +10,13 @@ def home(request):
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+class HomePageView(TemplateView):
+    template_name = 'home2.html'
+
+    
+    def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       context['minha_variavel'] = 'Ola, seja Bem vindo ao Curso de django advanced'
+       return context
+
