083c5dbdfe4aee6e2d5f74d6e04fe5676de06ac7
kaique <moraiskaique06@gmail.com>
2025-04-26 14:47:27 -0300

    Aula13 - UpdateView

--- Projeto_do_Curso_de_django/gestao_clientes/clientes/views.py
+++ Projeto_do_Curso_de_django/gestao_clientes/clientes/views.py
@@ -5,7 +5,8 @@ from .forms import PersonForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.utils import timezone
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 
 
 
@@ -63,3 +64,8 @@ class PersonCreate(CreateView):
     model = Person
     fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
     success_url = '/clientes/person_list'
+
+class PersonUpdate(UpdateView):
+    model = Person
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = reverse_lazy("person_list_cbv")
