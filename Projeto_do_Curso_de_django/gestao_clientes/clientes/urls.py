2003b7875457c60c5b1a802a1a0f9489aa813717
kaique <moraiskaique06@gmail.com>
2025-04-26 11:36:13 -0300

    aula11 - DetailViews

--- Projeto_do_Curso_de_django/gestao_clientes/clientes/urls.py
+++ Projeto_do_Curso_de_django/gestao_clientes/clientes/urls.py
@@ -4,6 +4,7 @@ from .views import persons_new
 from .views import persons_update
 from .views import persons_delete
 from .views import PersonList
+from .views import PersonDetail
 
 
 urlpatterns = [
@@ -11,5 +12,7 @@ urlpatterns = [
     path('new/', persons_new, name="person_new"),
     path('update/<int:id>/', persons_update, name="persons_update"),
     path('delete/<int:id>/', persons_delete, name="persons_delete"),
-    path('person_list', PersonList.as_view(), name="persons_delete")
+    path('person_list', PersonList.as_view()),
+    path('person_detail/<int:pk>/', PersonDetail.as_view(), name='person_detail_cbv'),
+
 ]
