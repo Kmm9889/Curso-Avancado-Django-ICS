71b7a0e342b53ce87fcb2ffba08138ac4d4c737d
kaique <moraiskaique06@gmail.com>
2025-05-24 16:17:44 -0300

    Aula21 - Refactors finais no template

--- Projeto_do_Curso_de_django/gestao_clientes/clientes/templatetags/filters.py
+++ Projeto_do_Curso_de_django/gestao_clientes/clientes/templatetags/filters.py
@@ -4,9 +4,5 @@ register = template.Library()
 
 
 @register.filter
-def my_filter(data):
-    return data + ' - ' + 'Alterado pelo filter'
-
-@register.filter
 def arredonda(value, casas):
     return round(value, casas)
