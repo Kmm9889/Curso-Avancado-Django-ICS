0025cc2c7588ebba576376497fab2762ab698c35
kaique <moraiskaique06@gmail.com>
2025-06-14 15:08:26 -0300

    Aula 26 - Ordenação de querysets e recuperação de models como dicionários

--- Projeto_do_Curso_de_django/gestao_clientes/gestao_clientes/aulas_no_terminal.py
+++ Projeto_do_Curso_de_django/gestao_clientes/gestao_clientes/aulas_no_terminal.py
@@ -1,3 +1,4 @@
 #Aula 22 - Esmiucando os models dentro do terminal
 #Aula 24 - Buscas avançadas no Banco de Dados
 #Aula 25 - Otimizando com values_list e prefetch_related
+#Aula 26 - Ordenação de querysets e recuperação de models como dicionários
