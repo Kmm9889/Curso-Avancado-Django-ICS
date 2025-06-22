d0bcc8e31868eb4d75e896c17775770dcdb6d70c
kaique <moraiskaique06@gmail.com>
2025-06-22 12:45:22 -0300

    Aula29 - Personalizando a exibição de fields no form

--- Projeto_do_Curso_de_django/gestao_clientes/clientes/admin.py
+++ Projeto_do_Curso_de_django/gestao_clientes/clientes/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
+class PersonAdmin(admin.ModelAdmin):
+    #fields = ('doc','first_name', 'last_name', 'age', 'salary', 'bio', 'photo')
+    exclude = ('bio',) 
+    list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'bio', 'photo')
 
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)
