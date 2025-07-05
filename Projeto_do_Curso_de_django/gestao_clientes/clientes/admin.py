9d5749dceb3b170bd5fe6666f5dba984d5584c64
kaique <moraiskaique06@gmail.com>
2025-07-05 16:11:00 -0300

    Aula38 - Melhorando a usabilidade de relacionamentos com Django auto_complete campos

--- Projeto_do_Curso_de_django/gestao_clientes/clientes/admin.py
+++ Projeto_do_Curso_de_django/gestao_clientes/clientes/admin.py
@@ -14,6 +14,8 @@ class PersonAdmin(admin.ModelAdmin):
     #exclude = ('bio',) 
     list_filter = ('age', 'salary')
     list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'bio', 'tem_foto')
+    search_fields = ('id', 'first_name')
+    autocomplete_fields = ['doc']
 
     def tem_foto(self, obj):
         if obj.photo:
@@ -25,12 +27,12 @@ class PersonAdmin(admin.ModelAdmin):
 
 class VendaAdmin(admin.ModelAdmin):
     readonly_fields = ('valor',)
-    raw_id_fields = ('pessoa',)
+    autocomplete_fields = ('pessoa', 'produtos',)   #raw_id_fields = ('pessoa',)
     list_filter = ('pessoa__doc', 'desconto')
     list_display = ('id', 'pessoa', 'get_total', 'nfe_emitida')
     search_fields = ('id', 'pessoa__first_name', 'pessoa__doc__num_doc')
     actions = [nfe_emitida, nfe_nao_emitida]
-    filter_horizontal = ['produtos']
+    #filter_horizontal = ['produtos']
 
     def total(self, obj):
         return obj.get_total()
@@ -39,8 +41,12 @@ class VendaAdmin(admin.ModelAdmin):
 
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('id', 'descricao', 'preco')
+    search_fields = ('id', 'descricao')
+
+class DocumentoAdmin(admin.ModelAdmin):
+    search_fields = ['num_doc']
 
 admin.site.register(Person, PersonAdmin)
-admin.site.register(Documento)
+admin.site.register(Documento, DocumentoAdmin)
 admin.site.register(Venda, VendaAdmin)
 admin.site.register(Produto, ProdutoAdmin)
