22db6c27df3e195d8c2735984f2f4a344e2c4310
kaique <moraiskaique06@gmail.com>
2025-07-13 11:56:35 -0300

    Aula40 - Refactorando a relação Produto Venda

--- Projeto_do_Curso_de_django/gestao_clientes/clientes/models.py
+++ Projeto_do_Curso_de_django/gestao_clientes/clientes/models.py
@@ -40,20 +40,30 @@ class Venda(models.Model):
     desconto = models.DecimalField(max_digits=5, decimal_places=2)
     impostos = models.DecimalField(max_digits=5, decimal_places=2)
     pessoa = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
-    produtos = models.ManyToManyField(Produto, blank=True)
     nfe_emitida = models.BooleanField(default=False)
 
-    def get_total(self):
-        tot = 0
-        for produto in self.produtos.all():
-            tot += produto.preco
-
-        return (tot - self.desconto) - self.impostos
+    #def get_total(self):
+    #    tot = 0
+    #    for produto in self.produtos.all():
+    #        tot += produto.preco
+    #
+    #    return (tot - self.desconto) - self.impostos
 
     def __str__(self):
         return self.numero
     
-@receiver(m2m_changed, sender=Venda.produtos.through)
+
+class ItensDoPedido(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.FloatField()
+    desconto = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.venda.numero + ' - ' + self.produto.descricao
+
+    
+#@receiver(m2m_changed, sender=Venda.produtos.through)
 def update_vendas_total(sender, instance, **kwargs):
     instance.valor = instance.get_total()
     instance.save()
