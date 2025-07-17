4d07badc2e96bf0a384cf99975ef81fe6f09705f
kaique <moraiskaique06@gmail.com>
2025-07-17 15:16:52 -0300

    Aula43 - Calculando o total do pedido com funções agregadas- SUM

--- Projeto_do_Curso_de_django/gestao_clientes/vendas/models.py
+++ Projeto_do_Curso_de_django/gestao_clientes/vendas/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 from django.db.models.signals import m2m_changed
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from clientes.models import Person
 from produtos.models import Produto
+from django.db.models import Sum, F, FloatField
 
 
 # Create your models here.
 class Venda(models.Model):
     numero = models.CharField(max_length=7)
     valor = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    desconto = models.DecimalField(max_digits=5, decimal_places=2)
-    impostos = models.DecimalField(max_digits=5, decimal_places=2)
+    desconto = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    impostos = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     pessoa = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
     nfe_emitida = models.BooleanField(default=False)
+
+    def calcular_total(self):
+        tot = self.itemdopedido_set.all().aggregate(
+            tot_ped=Sum((F('quantidade') * F('produto__preco')) - F('desconto'), output_field=FloatField())
+        )['tot_ped']
+
+        tot = tot - float(self.impostos) - float(self.desconto)
+        self.valor = tot
+        Venda.objects.filter(id=self.id).update(valor=tot)
+
+
 
     #def get_total(self):
     #    tot = 0
@@ -35,9 +48,14 @@ class ItemDoPedido(models.Model):
         return self.venda.numero + ' - ' + self.produto.descricao
 
     
-#@receiver(m2m_changed, sender=Venda.produtos.through)
+@receiver(post_save, sender=ItemDoPedido)
 def update_vendas_total(sender, instance, **kwargs):
-    instance.valor = instance.get_total()
-    instance.save()
+    instance.venda.calcular_total()
+
+
+@receiver(post_save, sender=Venda)
+def update_vendas_total2(sender, instance, **kwargs):
+    instance.calcular_total()
+    #instance.save()
 
     #Venda.objects.filter(id=instance.id).update(total=total)
