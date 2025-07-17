aa4e41e618de7bc67b74aa0b7e877451a79f25f7
kaique <moraiskaique06@gmail.com>
2025-07-17 15:41:29 -0300

    Aula 44 - Calculando total por item do pedido utilizando Annotate

--- Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_no_terminal.py
+++ Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_no_terminal.py
@@ -2,3 +2,4 @@
 #Aula 24 - Buscas avançadas no Banco de Dados
 #Aula 25 - Otimizando com values_list e prefetch_related
 #Aula 26 - Ordenação de querysets e recuperação de models como dicionários
+#Aula 44 - Calculando total por item do pedido utilizando Annotate
