f60c45526b893ad39b34aa91cac0513330bb8883
kaique <moraiskaique06@gmail.com>
2025-07-30 19:15:13 -0300

    Aula46 - Funções agregadas - Min, Max e Count

--- Projeto_do_Curso_de_django/gestao_clientes/vendas/views.py
+++ Projeto_do_Curso_de_django/gestao_clientes/vendas/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import View
-from django.db.models import Sum, F, FloatField, Max, Avg
+from django.db.models import Sum, F, FloatField, Max, Avg, Min, Count
 from .models import Venda
 
 class DashboardView(View):
     def get(self, request):
-        media = Venda.objects.all().aggregate(Avg('valor'))['valor__avg']
-        return render(request, 'vendas/dashboard.html', {'media': media})
+        data = {}
+        data['media'] = Venda.objects.all().aggregate(Avg('valor'))['valor__avg']
+        data['media_desc'] = Venda.objects.all().aggregate(Avg('desconto'))['desconto__avg']
+        data['max'] = Venda.objects.all().aggregate(Max('valor'))['valor__max']
+        data['min'] = Venda.objects.all().aggregate(Min('valor'))['valor__min']
+        data['n_ped'] = Venda.objects.all().aggregate(Count('id'))['id__count']
+        data['n_ped_nfe'] = Venda.objects.filter(
+            nfe_emitida=True
+        ).aggregate(Count('id'))['id__count']
+
+        return render(request, 'vendas/dashboard.html', data)
