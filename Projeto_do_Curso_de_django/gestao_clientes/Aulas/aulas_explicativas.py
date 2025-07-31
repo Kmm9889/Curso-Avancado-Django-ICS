0bb522a5de5c235da58e0fe5483cbe3a549d7633
kaique <moraiskaique06@gmail.com>
2025-07-31 18:15:10 -0300

    Aula 48 - Entendendo o sistema de Authentication and Authorization do Django

--- Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_explicativas.py
+++ Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_explicativas.py
@@ -1,1 +1,2 @@
 #Aula 39 - Entendendo o que e refactor e sua importância
+#Aula 48 - Entendendo o sistema de Authentication and Authorization do Django
--- Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_no_terminal.py
+++ Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_no_terminal.py
@@ -3,3 +3,4 @@
 #Aula 25 - Otimizando com values_list e prefetch_related
 #Aula 26 - Ordenação de querysets e recuperação de models como dicionários
 #Aula 44 - Calculando total por item do pedido utilizando Annotate
+#Aula 48 - Entendendo o sistema de Authentication and Authorization do Django
