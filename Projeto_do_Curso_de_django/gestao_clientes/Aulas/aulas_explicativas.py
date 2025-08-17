c79ab674d12fbb7f40759b4f020e9419ebc19f9f
kaique <moraiskaique06@gmail.com>
2025-08-17 15:32:07 -0300

    Aula53 - Finalizando o uso de Mixins

--- Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_explicativas.py
+++ Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_explicativas.py
@@ -1,3 +1,4 @@
 #Aula 39 - Entendendo o que e refactor e sua importância
 #Aula 48 - Entendendo o sistema de Authentication and Authorization do Django
 #as aulas 50 e 51 estão invertidas
+#Aula 53 - Finalizando o uso de Mixins
