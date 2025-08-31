bf745b20bb83f4bd7d37373ac874fe30cb029e8e
kaique <moraiskaique06@gmail.com>
2025-08-31 11:29:34 -0300

    Aula 56 - Introdução a cookies, seções e LocalStorage

--- Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_explicativas.py
+++ Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_explicativas.py
@@ -2,3 +2,4 @@
 #Aula 48 - Entendendo o sistema de Authentication and Authorization do Django
 #as aulas 50 e 51 estão invertidas
 #Aula 53 - Finalizando o uso de Mixins
+#Aula 56 - Introdução a cookies, seções e LocalStorage
