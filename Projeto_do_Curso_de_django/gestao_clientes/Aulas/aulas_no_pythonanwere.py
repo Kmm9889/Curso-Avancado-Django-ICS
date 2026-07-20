cdb02e1873c45e467b50a58474371025a590c7cc
kaique <moraiskaique06@gmail.com>
2026-07-20 14:29:41 -0300

    Aula69 - Deploy da aplicação e criação da App do Facebook

---
+++ Projeto_do_Curso_de_django/gestao_clientes/Aulas/aulas_no_pythonanwere.py
@@ -0,0 +1,4 @@
+#Aula 69 - Deploy da aplicação e criação da App do Facebook
+#
+#
+#
--- Projeto_do_Curso_de_django/gestao_clientes/gestao_clientes/settings.py
+++ Projeto_do_Curso_de_django/gestao_clientes/gestao_clientes/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-clientes2.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['KaiqueMonteiro.pythonanywhere.com', '127.0.0.1']
 
 INTERNAL_IPS = ["127.0.0.1",]
 
